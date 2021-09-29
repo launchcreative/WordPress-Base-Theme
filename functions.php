c4d09aba79da45f13478fbdbec1e9bdf58777c9d
Callum Strong <hello@callumstrong.co.uk>
2021-09-29 13:24:00 +0100

    Removed Custom Header functions

--- functions.php
+++ functions.php
@@ -151,11 +151,6 @@ function launch_creative_scripts() {
 add_action( 'wp_enqueue_scripts', 'launch_creative_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
